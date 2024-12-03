724556a5a39a0584c7c601733a6307323387e501
RuiXu <alexxuruin19991008@gmail.com>
2024-12-02 23:47:27 -0600

    2

---
+++ TEST3/Test3.js
@@ -0,0 +1,94 @@
+// Teachable Machine
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
+// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
+
+// The video
+let video;
+// For displaying the label
+let label = "waiting...";
+// The classifier
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/NmsqoV1m6/';
+let lastTime = 0;
+let interval = 1000;
+const audio1 = new Audio('1.mp4');
+const audio2 = new Audio('2.mp4');
+const audio3 = new Audio('3.mp4');
+const audio4 = new Audio('4.mp4');
+const audio5 = new Audio('5.mp4');
+const audio6 = new Audio('6.mp4');
+const audio7 = new Audio('7.mp4');
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+
+function setup() {
+  let canvas = createCanvas(800, 600);
+  canvas.parent('display-area'); 
+//Vedio catch!
+  video = createCapture(VIDEO);
+  video.size(800, 600);
+  video.hide(); 
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+
+// STEP 2 classify the videeo!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function draw() {
+  background(0);
+
+
+  // Draw the video
+  image(video, 0, 0);
+
+  // STEP 4: Draw the label
+  let currentTime = millis(); //
+  if (currentTime - lastTime >= interval) {
+    lastTime = currentTime;
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text(label, width / 2, height - 16);
+    if(label == "1") {
+      audio1.play();
+    }
+    else if (label == "2"){
+      audio2.play();
+    }
+    else if (label == "3"){
+      audio3.play();
+    }
+    else if (label == "4"){
+      audio4.play();
+    }
+    else if (label == "5"){
+      audio5.play();
+    }
+    else if (label == "6"){
+      audio6.play();
+    }
+    else if (label == "7"){
+      audio7.play();
+    }
+  }
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  // Something went wrong!
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label and classify again!
+  label = results[0].label;
+  classifyVideo();
+}
---
+++ Test2/Test2.js
@@ -0,0 +1,75 @@
+// Teachable Machine
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/TeachableMachine/3-teachable-audio
+// https://editor.p5js.org/codingtrain/sketches/e3nrNMG7A
+
+
+// Storing the label
+let label = "waiting...";
+
+// Classifier and model url
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/Pfgfz-MBi/';
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.soundClassifier(modelURL + 'model.json');
+}
+
+function setup() {
+  // Create a canvas and attach it to the HTML div with id 'display-area'
+  let canvas = createCanvas(800, 600);
+  canvas.parent('display-area'); 
+
+  // Set text alignment and styles for drawing on the canvas
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
+
+  // Start classifying (will listen to the microphone by default)
+  classifyAudio();
+}
+
+// STEP 2 classify!
+function classifyAudio() {
+  classifier.classify(gotResults);
+}
+
+function draw() {
+  background(0);
+
+  // STEP 4: Draw the label
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width/2, height - 16);
+
+  // Background noise is headphones
+  // let emoji = "🎧";
+  // Pick an emoji based on label
+  // if (label == "Train") {
+  //   emoji = "🚂";
+  // } else if (label == "Bell") {
+  //   emoji = "🛎";
+  // } else if (label == "Ukulele") {
+  //   emoji = "🎸";
+  // }
+
+  // Draw the emoji
+  // textSize(256);
+  // text(emoji, width / 2, height / 2);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log("Got results:", results); 
+
+  // Store the label
+  label = results[0].label;
+
+classifyAudio(); // Continue to classify the next input
+}
